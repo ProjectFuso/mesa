4fbf252b56a7020d3ad77f6d529bacae355687d4
Youkou Tenhouin <youkou@tenhou.in>
2026-04-06 14:38:28 +0200

    ProjectFuso: fix condition in `util_thread_get_time_nano`

--- src/util/u_thread.c
+++ src/util/u_thread.c
@@ -150,7 +150,7 @@ util_set_thread_affinity(thrd_t thread,
 int64_t
 util_thread_get_time_nano(thrd_t thread)
 {
-#if defined(HAVE_PTHREAD) && !defined(__APPLE__) && !defined(__HAIKU__) && !defined(__managarm__)
+#if defined(HAVE_PTHREAD) && !defined(__APPLE__) && !defined(__HAIKU__) && !defined(__managarm__) && !defined(__illumos__)
    struct timespec ts;
    clockid_t cid;
 
